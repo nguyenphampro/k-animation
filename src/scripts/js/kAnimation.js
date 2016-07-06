8d26dc0c0623303722754998de2d783e2bfffda0
Nguyen Pham <Nguyen Pham>
2016-07-07 00:07:17 +0700

    up

--- src/scripts/js/kAnimation.js
+++ src/scripts/js/kAnimation.js
@@ -7,60 +7,158 @@
 
 // http://debuggable.com/posts/how-to-write-jquery-plugins:4f72ab2e-7310-4a74-817a-0a04cbdd56cb
 
+
 ;
 (function($, window, document, undefined) {
-
-    var kA = "kAnimation",
-        key = "ka_" + kA;
+    var kA = 'kAnimation',
+        key = 'ka_' + kA
 
     var kaGlobal = function(element, options) {
-        this.element = element;
+        this.element = element
 
         // OPTIONS
         this.options = {
-            ClassName: 'kAnimation',
-            color: '#999'
-        };
+            ClassName: 'animated',
+            Animation: 'fadeIn',
+            Type: 'auto', // auto, scroll, click, hover
+            Delay: '0',
+            ScrollLoop: true
+        }
 
-        this.init(options);
-    };
+        this.init(options)
+    }
 
     kaGlobal.prototype = {
         // Initial 
         init: function(options) {
-            $.extend(this.options, options);
+            $.extend(this.options, options)
+            var $e = this.element,
+                $o = this.options,
+                $d = this.options.Delay,
+                $sl = this.options.ScrollLoop,
+                $t = (this.options.Type).toLocaleLowerCase()
+
+            // console.log(typeof($o.Animation))
+            // console.log($o)
+
+
             // DO Animation 
-            this.element.addClass(this.options.ClassName);
-            this.element.css({
-                'color': this.options.color,
-            });
-        },
-        // More Options
-        on: function(str) {
-            if (str === 'change') {
-                this.options.color = str;
-                this.element.css('color', str);
-            } else if (str === 'leave') {
-                this.options.color = str;
-                this.element.css('color', '#fff');
+            var doAnimation = function(remove) {
+                if (remove === 'remove') {
+                    $e.removeClass($o.ClassName)
+                } else {
+                    $e.addClass($o.ClassName)
+                }
+
+                if (typeof($o.Animation) == 'string') {
+                    if (remove === 'remove') {
+                        $e.removeClass($o.Animation)
+                    } else {
+                        setTimeout(function() {
+                            $e.addClass($o.Animation)
+                        }, $d)
+                    }
+                } else {
+                    var i = 1
+                    var indexArray = $o.Animation
+                    var timerx = []
+                    $(indexArray).each(function(indexArraykey) {
+                        function internalCallback(i, indexArraykey) {
+                            return function() {
+                                var val = indexArray[indexArraykey]
+                                if (remove === 'remove') {
+                                    $e.removeClass(val)
+                                } else {
+                                    $e.addClass(val)
+                                }
+                            }
+                        }
+                        if (remove === 'remove') {
+                            timerx[i] = setTimeout(internalCallback(i, indexArraykey), i)
+                        } else {
+                            timerx[i] = setTimeout(internalCallback(i, indexArraykey), i * $d)
+                        }
+                        i++
+                    })
+                }
+            }
+
+            // Scroll
+            if ($t === 'scroll') {
+                var $w = $(window)
+                    // Xem lai ham nay 
+                function check_if_in_view() {
+                    var window_height = $w.height()
+                    var window_top_position = $w.scrollTop()
+                    var window_bottom_position = (window_top_position + window_height)
+
+                    $.each($e, function() {
+                        var $element = $(this)
+                        var element_height = $element.outerHeight()
+                        var element_top_position = $element.offset().top
+                        var element_bottom_position = (element_top_position + element_height)
+
+                        if ((element_bottom_position >= window_top_position) &&
+                            (element_top_position <= window_bottom_position)) {
+                            doAnimation()
+                        } else {
+                            if ($sl) {
+                                doAnimation('remove')
+                            }
+                        }
+                    })
+                }
+
+                $w.on('scroll resize', check_if_in_view)
+            }
+            // Click
+            else if ($t === 'click') {
+                $e.each(function(index) {
+                    $(this).on('click', function() {
+                        doAnimation()
+                    })
+                    if ($sl) {
+                        console.log(1);
+                    } else {
+                        console.log(3);
+                    }
+                })
+            }
+            // Click
+            else if ($t === 'hover') {
+                $e.each(function(index) {
+                    $(this).hover(function() {
+                        doAnimation()
+                    })
+                })
+            } else {
+                doAnimation()
             }
         },
+        // More Options
+        // on: function(str) {
+        //     if (str === 'change') {
+        //         this.options.color = str
+        //         this.element.css('color', str)
+        //     } else if (str === 'leave') {
+        //         this.options.color = str
+        //         this.element.css('color', '#fff')
+        //     }
+        // },
 
-    };
-
+    }
 
     // Build Animation 
     $.fn[kA] = function(options) {
-        var jQuerykAnimation = this.data(key);
+        var jQuerykAnimation = this.data(key)
         if (jQuerykAnimation instanceof kaGlobal) {
             if (typeof options !== 'undefined') {
-                jQuerykAnimation.init(options);
+                jQuerykAnimation.init(options)
             }
         } else {
-            jQuerykAnimation = new kaGlobal(this, options);
-            this.data(key, jQuerykAnimation);
+            jQuerykAnimation = new kaGlobal(this, options)
+            this.data(key, jQuerykAnimation)
         }
-        return jQuerykAnimation;
-    };
-
-}(jQuery, window, document));
+        return jQuerykAnimation
+    }
+}(jQuery, window, document))
